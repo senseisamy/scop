b4f5814576eb78bad49f520fe10e2eccf6da1145
SenseiSamy <samy.naji2003@gmail.com>
2025-01-07 15:47:30 +0100

    erhdrjfj

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 #![allow(dead_code, unused)]
 
-mod app;
+mod vulkan;
+mod window;
 mod object;
 
 use anyhow::{bail, Result};
-use log::*;
-use app::App;
 use object::Object;
 use std::env;
 use std::fs;
@@ -17,19 +16,12 @@ use winit::event_loop::EventLoop;
 use winit::window::Window;
 
 fn main() -> Result<()> {
-    pretty_env_logger::init();
-
     let args: Vec<String> = env::args().collect();
     let file = fs::read_to_string(&args[1])?;
     let object = match Object::from_str(&file) {
         Ok(object) => object,
         Err(_) => bail!("Failed to parse the obj file"),
     };
-
-    let event_loop = EventLoop::new()?;
-    event_loop.set_control_flow(ControlFlow::Poll);
-    let mut app = App::default();
-    event_loop.run_app(&mut app)?;
 
     Ok(())
 }
